c1cdb17eac511e3ea449473f12161c34ea9d79c2
Adam Bachrach <nalc1100adam@gmail.com>
2022-08-04 21:28:06 -0400

    add cors

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ import mongoose from 'mongoose'
 import express from 'express'
 import db from './connection.js'
 import Quote from './Quote.js'
+import cors from 'cors'
 
 const app = express()
 const PORT = process.env.PORT || 4000
 
 app.use(express.json())
+app.use(cors())
 
 app.listen(PORT, () => {
   console.log(`Successfully listening on Port ${PORT}`)
